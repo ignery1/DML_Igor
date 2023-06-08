9160c2b266a389a17dd48240a12962cec7bdd517
ignery1 <106778230+ignery1@users.noreply.github.com>
2023-06-08 15:38:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,42 @@
+
+
 import pandas as pd
 import streamlit as st
-import joblib
+from pycaret.regression import *
+import numpy as np
 
-# Carregando o modelo treinado
-model = joblib.load('model.pkl')
+#Load the trained model
+model = load_model('model')
 
-# Carregando os dados dos estudantes
+#Load the dataset
 dataset = pd.read_csv('estudantes.csv')
 
-# Criando um dicionário para mapear os valores das variáveis categóricas
-categorical_mapping = {
-    'female': {'Feminino': 1, 'Masculino': 0},
-    'A': {'A': 1, 'B': 0, 'C': 0, 'D': 0, 'E': 0},
-    'AD': {'AD': 1, 'BD': 0, 'HS': 0, 'SC': 0, 'SHS': 0},
-    'completed': {'Completo': 1, 'Nenhum': 0},
-    'FR': {'Gratuito/Reduzido': 1, 'Padrão': 0}
-}
+#Create a sidebar with input fields for the student's attributes
+st.sidebar.subheader('Define the student attributes for math score prediction')
 
-# Criando a interface do Streamlit
-st.title("Predição de notas de matemática")
-st.markdown("Este é um aplicativo que realiza a predição da nota de matemática com base nos atributos do aluno.")
+reading_score = st.sidebar.number_input('Reading score')
+writing_score = st.sidebar.number_input('Writing score')
+gender = st.sidebar.selectbox('Gender', ('Female', 'Male'))
+ethnicity = st.sidebar.selectbox('Ethnicity', ('A', 'B', 'C', 'D', 'E'))
+parental_education = st.sidebar.selectbox('Parental education', ('BD', 'SC', 'MD', 'AD', 'HS', 'SHS'))
+test_preparation_course = st.sidebar.selectbox('Test preparation course', ('None', 'Complete'))
+lunch = st.sidebar.selectbox('Lunch', ('Free/Reduced', 'Standard'))
 
-# Criando os campos de entrada para os atributos do aluno
-atributos_aluno = {
-    'nota_leitura': st.sidebar.number_input("Nota de Leitura"),
-    'nota_escrita': st.sidebar.number_input("Nota de Escrita"),
-    'female': st.sidebar.selectbox("Gênero do Aluno", ('Feminino', 'Masculino')),
-    'A': st.sidebar.selectbox("Raça/Etnia", ('A', 'B', 'C', 'D', 'E')),
-    'AD': st.sidebar.selectbox("Grau de Escolaridade", ('BD', 'SC', 'MD', 'AD', 'HS', 'SHS')),
-    'completed': st.sidebar.selectbox("Curso Preparatório para Teste", ('Nenhum', 'Completo')),
-    'FR': st.sidebar.selectbox("Tipo de Almoço", ('Gratuito/Reduzido', 'Padrão'))
-}
+#Convert the input data to a Pandas DataFrame
+data_test = pd.DataFrame({
+'reading_score': [reading_score],
+'writing_score': [writing_score],
+'gender': [gender],
+'ethnicity': [ethnicity],
+'parental_education': [parental_education],
+'test_preparation_course': [test_preparation_course],
+'lunch': [lunch]
+})
 
-# Selecionando as colunas relevantes do dataset
-colunas_relevantes = ['nota_leitura', 'nota_escrita', 'female', 'A', 'AD', 'completed', 'FR']
-data_teste = dataset[colunas_relevantes].copy()
+#Make a prediction
+prediction = model.predict(data_test)
 
-# Preenchendo o data_teste com base nos atributos do aluno
-data_teste = data_teste.append(atributos_aluno, ignore_index=True)
+#Display the prediction
+st.subheader('Predicted math score:')
+st.write(round(prediction[0], 2))
 
-# Mapeando os valores categóricos para numéricos
-for col, mapping in categorical_mapping.items():
-    data_teste[col] = data_teste[col].map(mapping)
-
-# Realizando a predição quando o botão for acionado
-btn_predict = st.sidebar.button("Realizar Predição")
-
-if btn_predict:
-    result = model.predict(data_teste.tail(1))
-    result = round(result[0], 2)
-
-    st.subheader("Nota de matemática predita:")
-    st.write(result)
