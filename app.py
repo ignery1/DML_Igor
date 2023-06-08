57857b6326e164dba481f88c1e4f31f3bb4d8cdf
ignery1 <106778230+ignery1@users.noreply.github.com>
2023-06-08 15:47:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,48 @@
 import pandas as pd
 import streamlit as st
-import joblib
+from pycaret.regression import load_model
 
-# Carregando o modelo treinado
-model = joblib.load('model.pkl')
+# Carregar o modelo treinado
+model = load_model('model')
 
-# Carregando os dados dos estudantes
-dataset = pd.read_csv('estudantes.csv')
-print(dataset.columns)
+# Carregar o conjunto de dados
+dataset = pd.read_csv('estudantes.csv') 
 
-
-# Criando um dicionário para mapear os valores das variáveis categóricas
-categorical_mapping = {
-    'female': {'Feminino': 1, 'Masculino': 0},
-    'A': {'A': 1, 'B': 0, 'C': 0, 'D': 0, 'E': 0},
-    'AD': {'AD': 1, 'BD': 0, 'HS': 0, 'SC': 0, 'SHS': 0},
-    'completed': {'Completo': 1, 'Nenhum': 0},
-    'FR': {'Gratuito/Reduzido': 1, 'Padrão': 0}
-}
-
-# Criando a interface do Streamlit
+# Configurar a interface do Streamlit
 st.title("Predição de notas de matemática")
-st.markdown("Este é um aplicativo que realiza a predição da nota de matemática com base nos atributos do aluno.")
+st.markdown("Este é um aplicativo de dados usado para prever notas de matemática usando Machine Learning.")
 
-# Criando os campos de entrada para os atributos do aluno
-atributos_aluno = {
-    'nota_leitura': st.sidebar.number_input("Nota de Leitura"),
-    'nota_escrita': st.sidebar.number_input("Nota de Escrita"),
-    'female': st.sidebar.selectbox("Gênero do Aluno", ('Feminino', 'Masculino')),
-    'A': st.sidebar.selectbox("Raça/Etnia", ('A', 'B', 'C', 'D', 'E')),
-    'AD': st.sidebar.selectbox("Grau de Escolaridade", ('BD', 'SC', 'MD', 'AD', 'HS', 'SHS')),
-    'completed': st.sidebar.selectbox("Curso Preparatório para Teste", ('Nenhum', 'Completo')),
-    'FR': st.sidebar.selectbox("Tipo de Almoço", ('Gratuito/Reduzido', 'Padrão'))
-}
+# Definir os atributos do aluno para a predição da nota de matemática
+nota_leitura = st.sidebar.number_input("Nota de Leitura")
+nota_escrita = st.sidebar.number_input("Nota de Escrita")
+genero = st.sidebar.selectbox("Gênero do Aluno", ("Feminino", "Masculino"))
+etinia = st.sidebar.selectbox("Raça/Etnia", ("A", "B", "C", "D", "E"))
+educacao_pais = st.sidebar.selectbox("Grau de Escolaridade dos Pais", ("BD", "SC", "MD", "AD", "HS", "SHS"))
+curso_preparacao = st.sidebar.selectbox("Curso Preparatório para Teste", ("Nenhum", "Completo"))
+almoco = st.sidebar.selectbox("Tipo de Almoço", ("Gratuito/Reduzido", "Padrão"))
 
-# Selecionando as colunas relevantes do dataset
-colunas_relevantes = ['nota_leitura', 'nota_escrita', 'female', 'A', 'AD', 'completed', 'FR']
-#data_teste = dataset[colunas_relevantes].copy()
-#data_teste = dataset.loc[:, colunas_relevantes].copy()
-data_teste = dataset[colunas_relevantes].copy()
+# Mapear os valores selecionados para os atributos correspondentes
+gender_mapping = {"Feminino": 1, "Masculino": 0}
+ethnicity_mapping = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
+education_mapping = {"BD": 1, "SC": 2, "MD": 3, "AD": 4, "HS": 5, "SHS": 6}
+prep_course_mapping = {"Nenhum": 0, "Completo": 1}
+lunch_mapping = {"Gratuito/Reduzido": 0, "Padrão": 1}
 
-# Preenchendo o data_teste com base nos atributos do aluno
-data_teste = data_teste.append(atributos_aluno, ignore_index=True)
+# Criar o dataframe de teste com os valores selecionados
+data_teste = pd.DataFrame({
+    "nota_leitura": [nota_leitura],
+    "nota_escrita": [nota_escrita],
+    "gender": [gender_mapping[genero]],
+    "ethnicity": [ethnicity_mapping[etinia]],
+    "parental_education": [education_mapping[educacao_pais]],
+    "prep_course": [prep_course_mapping[curso_preparacao]],
+    "lunch": [lunch_mapping[almoco]]
+})
 
-# Mapeando os valores categóricos para numéricos
-for col, mapping in categorical_mapping.items():
-    data_teste[col] = data_teste[col].map(mapping)
+# Realizar a predição usando o modelo carregado
+result = model.predict(data_teste)
+predicted_score = round(result[0], 2)
 
-# Realizando a predição quando o botão for acionado
-btn_predict = st.sidebar.button("Realizar Predição")
-
-if btn_predict:
-    result = model.predict(data_teste.tail(1))
-    result = round(result[0], 2)
-
-    st.subheader("Nota de matemática predita:")
-    st.write(result)
+# Exibir o resultado da predição
+st.subheader("Nota de Matemática Predita:")
+st.write(predicted_score)
