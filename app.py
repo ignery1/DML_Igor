8c9f64b75daa85a004616e4c829b9dd1ce6e6d8c
ignery1 <106778230+ignery1@users.noreply.github.com>
2023-06-08 15:49:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ nota_leitura = st.sidebar.number_input("Nota de Leitura")
 nota_escrita = st.sidebar.number_input("Nota de Escrita")
 genero = st.sidebar.selectbox("Gênero do Aluno", ("Feminino", "Masculino"))
 etinia = st.sidebar.selectbox("Raça/Etnia", ("A", "B", "C", "D", "E"))
-educacao_pais = st.sidebar.selectbox("Grau de Escolaridade dos Pais", ("BD", "SC", "MD", "AD", "HS", "SHS"))
+educacao_pais = st.sidebar.selectbox("Grau de Escolaridade dos Pais", ("AD", "BD", "HS", "SC", "SHS"))
 curso_preparacao = st.sidebar.selectbox("Curso Preparatório para Teste", ("Nenhum", "Completo"))
 almoco = st.sidebar.selectbox("Tipo de Almoço", ("Gratuito/Reduzido", "Padrão"))
 
 # Mapear os valores selecionados para os atributos correspondentes
 gender_mapping = {"Feminino": 1, "Masculino": 0}
 ethnicity_mapping = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
-education_mapping = {"BD": 1, "SC": 2, "MD": 3, "AD": 4, "HS": 5, "SHS": 6}
+education_mapping = {"AD": 1, "BD": 2, "HS": 3, "SC": 4, "SHS": 5}
 prep_course_mapping = {"Nenhum": 0, "Completo": 1}
 lunch_mapping = {"Gratuito/Reduzido": 0, "Padrão": 1}
 
@@ -32,17 +32,28 @@ lunch_mapping = {"Gratuito/Reduzido": 0, "Padrão": 1}
 data_teste = pd.DataFrame({
     "nota_leitura": [nota_leitura],
     "nota_escrita": [nota_escrita],
-    "gender": [gender_mapping[genero]],
-    "ethnicity": [ethnicity_mapping[etinia]],
-    "parental_education": [education_mapping[educacao_pais]],
-    "prep_course": [prep_course_mapping[curso_preparacao]],
-    "lunch": [lunch_mapping[almoco]]
+    "female": [gender_mapping[genero]],
+    "male": [1 - gender_mapping[genero]],
+    "A": [1 if etinia == "A" else 0],
+    "B": [1 if etinia == "B" else 0],
+    "C": [1 if etinia == "C" else 0],
+    "D": [1 if etinia == "D" else 0],
+    "E": [1 if etinia == "E" else 0],
+    "AD": [1 if educacao_pais == "AD" else 0],
+    "BD": [1 if educacao_pais == "BD" else 0],
+    "HS": [1 if educacao_pais == "HS" else 0],
+    "SC": [1 if educacao_pais == "SC" else 0],
+    "SHS": [1 if educacao_pais == "SHS" else 0],
+    "FR": [1 if almoco == "Gratuito/Reduzido" else 0],
+    "S": [1 if almoco == "Padrão" else 0],
+    "completed": [prep_course_mapping[curso_preparacao]],
+    "none": [1 - prep_course_mapping[curso_preparacao]]
 })
 
-# Realizar a predição usando o modelo carregado
+# Realizar a predição
 result = model.predict(data_teste)
-predicted_score = round(result[0], 2)
+predicted_math_score = round(result[0], 2)
 
-# Exibir o resultado da predição
+# Exibir a nota de matemática predita
 st.subheader("Nota de Matemática Predita:")
-st.write(predicted_score)
+st.write(predicted_math_score)
